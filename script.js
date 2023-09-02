380c792ee5733e96914e33af9c6ad671d8b896a3
Alex Garza <preemo956@gmail.com>
2023-09-02 13:34:31 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,39 @@
+import { localDB } from 'https://cdn.skypack.dev/peadb'
+import shortid from 'https://cdn.skypack.dev/shortid'
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
+
+const db = new localDB('grocery-list-db')
+const groceries = db.getAll() || []
+
+const groceryList = document.getElementById('groceryList')
+const newGroceryInput = document.getElementById('newGrocery')
+const addBtn = document.getElementById('addBtn')
+
+const createGroceryElement = grocery => {
+    const groceryElement = document.createElement('li')
+    groceryElement.innerText = grocery.value
+    groceryElement.classList.add('groceryItem')
+    groceryElement.addEventListener('click', () => {
+        groceryElement.remove()
+        db.delete(grocery.key)
+        confetti({ particleCount: 300, spread: 1000, origin: { y: 1}})
+    })
+    return groceryElement
+}
+
+const addGrocery = newGrocery => {
+    groceryList. appendChild(createGroceryElement(newGrocery))
+}
+
+addBtn.addEventListener('click', e => {
+    e.preventDefault()
+    const Value = newGroceryInput.value
+    if (value) {
+        const key = shortid.getElementById()
+        addGrocery({ key, value })
+        db.set(key, value)
+    newGroceryInput.value = null
+}
+})
+
+groceries.map(grocery=>addGrocery(grocery))
